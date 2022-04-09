a0cec659e6879c6c3e38084da42a8df4cdfe3f2b
joao21dev <joao21dev@gmail.com>
2022-04-09 10:12:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import Sobre from "./Sobre";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Contato from "./Contato";
+import Campanhas from "./Campanhas";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Route path="/" exact component={Home} />
+          <Route path="/sobre" component={Sobre} />
+          <Route path="/campanhas" component={Campanhas} />
+          <Route path="/contato" component={Contato} />
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer text-faded text-center py-5">
+      <div className="container">
+        <p className="m-0 small">
+          <a href="https://www.devpleno.com/software-do-bem">
+            <img src="img/software-do-bem-logo-320x128.png" />
+          </a>
+          <br />
+          joao21dev
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
